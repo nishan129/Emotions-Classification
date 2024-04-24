dfd1e5a51967fc2b85db31be42294d7652c956e0
nishant129 <nishantborkar139@gmail.com>
2024-04-24 09:46:06 +0530

    Torch model upload

--- src/emotion/ml/pt_model.py
+++ src/emotion/ml/pt_model.py
@@ -9,6 +9,9 @@ from sklearn.model_selection import train_test_split
 import random
 
 
+train_dataloader = DataLoader( batch_size=32, shuffle=True)
+val_dataloader = DataLoader(batch_size=32)
+
 class EmotionClassifierWithConv(nn.Module):
     def __init__(self, transformer_model, num_classes, kernel_size=3, num_filters=256):
         super(EmotionClassifierWithConv, self).__init__()
@@ -25,3 +28,80 @@ class EmotionClassifierWithConv(nn.Module):
         pooled_conv_out, _ = torch.max(conv_out, dim=2)  
         logits = self.fc(pooled_conv_out)
         return logits
+    
+    
+    
+    
+num_classes =6  
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device_ids = [0, 1]  
+
+
+
+model = EmotionClassifierWithConv(model, num_classes)
+model = nn.DataParallel(model, device_ids=device_ids)
+model = model.to(device)
+
+
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+criterion = nn.CrossEntropyLoss()
+
+
+num_epochs = 3
+
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0.0
+    total_correct = 0
+    
+    
+    for batch in train_dataloader:
+        input_ids, attention_mask, label = batch
+        
+        input_ids, attention_mask, label = input_ids.to(device), attention_mask.to(device), label.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(input_ids, attention_mask)
+        
+        labels = label.to(device).long()
+        outputs = outputs.float()
+        
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        
+        total_loss += loss.item()
+        _, predicted = torch.max(outputs,1)
+        total_correct += (predicted == labels).sum().item()
+        
+    train_loss = total_loss/ len(train_dataloader)
+    train_accuracy = total_correct / len(train_dataloader)
+    
+    #validation step
+    model.eval()
+    total_val_loss = 0.0
+    total_val_correct = 0
+    val_predicted = []
+    val_labels = []
+    
+    
+    with torch.no_grad():
+        for batch in val_dataloader:
+            input_ids, attention_mask, labels = batch
+            
+            input_ids, attention_mask, labels = input_ids.to(device), attention_mask.to(device), labels.to(device)
+            
+            outputs = model(input_ids, attention_mask)
+            labels = labels.to(device).long()
+            
+            outputs = outputs.float()
+            
+            loss = criterion(outputs, labels)
+            
+            total_val_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total_val_correct += (predicted == labels).sum().item()
+            val_predicted.extend(predicted.cpu().numpy())
+            val_labels.extend(labels.cpu().numpy())
--- src/emotion/ml/tf_model.py
+++ src/emotion/ml/tf_model.py
@@ -2,3 +2,21 @@ from transformers import AutoTokenizer
 import tensorflow as tf
 from src.emotion import ModelException
 import sys
+import tensorflow_hub as hub
+
+
+bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
+
+
+# Bert layers
+text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+preprocessed_text = bert_preprocess(text_input)
+outputs = bert_encoder(preprocessed_text)
+
+# Neural network layers
+l = tf.keras.layers.Dropout(0.1, name="dropout")(outputs['pooled_output'])
+l = tf.keras.layers.Dense(1, activation='sigmoid', name="output")(l)
+
+# Use inputs and outputs to construct a final model
+model = tf.keras.Model(inputs=[text_input], outputs = [l])
